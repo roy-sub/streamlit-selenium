f6bee0789b5d6e41c9b5dad99e9eec20e202f100
Subhradip Roy <subhrastien@gmail.com>
2024-04-27 16:45:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def run_selenium(url):
     options = Options()
