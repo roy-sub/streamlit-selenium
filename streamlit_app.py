4b0998a92aee8389cdc16f29f7305cd193a9064e
Subhradip Roy <subhrastien@gmail.com>
2024-04-27 15:17:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager import ChromeType
+from webdriver_manager.chrome import ChromeType
 
 @st.cache(allow_output_mutation=True)
 def get_driver():
