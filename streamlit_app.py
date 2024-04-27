f6bf48bd4205aed1b7ff9c6e835bd7bc228ed940
Subhradip Roy <subhrastien@gmail.com>
2024-04-27 16:11:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,10 @@ def get_driver(options):
 def get_text_from_url(url):
     
     options = Options()
-    
+    options.add_argument("--disable-gpu")
+    # Comment out the following line to run in non-headless mode
+    # options.add_argument("--headless")
+
     driver = get_driver(options)
     driver.get(url)
     
