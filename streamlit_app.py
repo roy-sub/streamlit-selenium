6d8e1a8b353cf3f421a84c1bbee695edd5a46e9a
Subhradip Roy <subhrastien@gmail.com>
2024-04-27 16:20:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,31 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
+import os
+import time
+import json
 import streamlit as st
 
-@st.cache_resource
-def get_driver():
-    return webdriver.Chrome(
-        service=Service(
-            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-        ),
-        options=options,
-    )
+os.chmod('chromedriver',0o755)
 
-options = Options()
-options.add_argument("--disable-gpu")
-options.add_argument("--headless")
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 
-driver = get_driver()
-driver.get("http://example.com")
+PROXY = "proxy.soax.com:9000"
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('proxy.soax.com'.format(PROXY))
 
-st.code(driver.page_source)
+driver = webdriver.Chrome('chromedriver', options=chrome_options) 
+driver.get("https://www.instagram.com/accounts/login/")
+
+page_title = driver.title
+
+time.sleep(5)
+driver.quit()
+
+# Display the extracted content in Streamlit
+st.title('Web Page Content Display')
+st.write(f'Page Title: {page_title}')
