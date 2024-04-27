58ffdcd374470bbe14df71e83ddb3cc55864a167
Subhradip Roy <subhrastien@gmail.com>
2024-04-27 17:11:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,9 @@ def run_selenium(url):
     driver.get(url)
 
     # Wait for the entire page to be loaded
-    username_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "username"))) 
-    password_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password")))
-    login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[text()='Log in']")))
+    username_input = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.NAME, "username"))) 
+    password_input = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.NAME, "password")))
+    login_button = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//div[text()='Log in']")))
     
     content = driver.page_source
     driver.quit()
