36988923629c96c1dfcd8312974d67d78b230005
Subhradip Roy <subhrastien@gmail.com>
2024-04-27 16:16:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
+import streamlit as st
 
 @st.cache_resource
 def get_driver():
