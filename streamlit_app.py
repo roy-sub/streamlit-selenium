5b9591ac0fb037103d010d9b750dfe48f40663be
Subhradip Roy <subhrastien@gmail.com>
2024-05-01 03:07:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,10 +29,10 @@ def run_selenium(url):
     while(keepScrolling):
         
         divSideBar.send_keys(Keys.PAGE_DOWN)
-        time.sleep(10)
+        time.sleep(30)
         
         divSideBar.send_keys(Keys.PAGE_DOWN)
-        time.sleep(10)
+        time.sleep(30)
         
         html =driver.find_element(By.TAG_NAME, "html").get_attribute('outerHTML')
         
