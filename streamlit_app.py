31c771297c68b5790c7c566692f58e6a9631a63e
Subhradip Roy <subhrastien@gmail.com>
2024-05-01 13:32:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,18 +19,6 @@ def run_selenium(url):
 
     # Scroll Down
 
-    last_height = driver.execute_script("return document.body.scrollHeight")
-    while True:
-        time.sleep(5)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        time.sleep(5)
-        new_height = driver.execute_script("return document.body.scrollHeight")
-
-        if new_height == last_height:
-            break
-        last_height = new_height
-
     # Scrape
 
     elements = driver.find_elements(By.CLASS_NAME, "hfpxzc")
